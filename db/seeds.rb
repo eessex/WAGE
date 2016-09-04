e77c5fcc23afd13dc4f551d8f7585247d1b43a6e
Eve <eve.essex@gmail.com>
2016-09-03 20:10:41 -0400

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,8 @@ Employee.create(name: 'Joe Doe', email: 'joe@doe.com', manager: false)
 Employee.create(name: 'Jane Doe', email: 'jane@doe.com', manager: true)
 Employee.create(name: 'Bob Dole', email: 'bob@dole.com', manager: false)
 Employee.create(name: 'John Doe', email: 'john@doe.com', manager: true)
+issue = User.create(institution_name: "ISSUE Project Room", email: "test@issueprojectroom.org", password: "password", password_confirmation: "password")
+artistsspace = User.create(institution_name: "Artists Space", email: "test@artistsspace.org", password: "password", password_confirmation: "password")
+
+
+cert1 = Certification.create(user_id: 1, fiscal_start: "2015-01-01", fiscal_end: "2015-12-31", operating_expenses: 500000)
