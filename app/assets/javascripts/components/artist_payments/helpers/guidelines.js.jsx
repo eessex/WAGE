0518ab9338146136f3a78d7fbf5de778353b4b44
Eve <eve.essex@gmail.com>
2016-11-06 15:28:22 -0500

    update homepage

--- app/assets/javascripts/components/artist_payments/helpers/guidelines.js.jsx
+++ app/assets/javascripts/components/artist_payments/helpers/guidelines.js.jsx
@@ -3,18 +3,19 @@ var Guidelines = React.createClass({
     return {
       isFuture: true,
       isPast: false,
-      isProgress: false
+      isProgress: false,
+      isGeneral: true
     }
   },
   componentDidMount() {
     if (this.props.getYearStatus().future != null) {
-      this.setState({isFuture: this.props.getYearStatus().future})
+      this.setState({isFuture: this.props.getYearStatus().future, isGeneral: false})
     }
     if (this.props.getYearStatus().past != null) {
-      this.setState({isPast: this.props.getYearStatus().past})
+      this.setState({isPast: this.props.getYearStatus().past, isGeneral: false})
     }
     if (this.props.getYearStatus().progress != null) {
-      this.setState({isProgress: this.props.getYearStatus().progress})
+      this.setState({isProgress: this.props.getYearStatus().progress, isGeneral: false})
     }
   },
   printYearStatus() {
@@ -36,28 +37,30 @@ var Guidelines = React.createClass({
   },
   getSections() {
     var section
-    if ((this.state.isFuture || this.state.isProgress) && (this.state.isPast == null)) {
-    section = <div>
-      <h4>Please note that your upcoming fiscal year must begin within 2-3 months of this application. In addition to the documentation already submitted, we will need the following:</h4>
-      <ul>
-        <li>Total operating expenses for the year</li>
-        <li>A projected operating budget</li>
-      </ul>
-      <h4>Your application is effectively a commitment to paying fees according to W.A.G.E. standards. Your status will be listed as W.A.G.E. Certified Pending until the close of the fiscal period, at which time a history of fee payments is required. Once you have demonstrated your organization’s compliance you will be listed and may list yourself as W.A.G.E. Certified.
-      </h4>
-    </div>
-  } else {
-    section = <div>
-      <h4>To process your application we will need the following documentation about that fiscal year in order to establish that fees were paid according to W.A.G.E. standards:</h4>
-      <h4>
-      <ul>
-        <li>Total operating expenses for that year</li>
-        <li>A history of fee payment</li>
-        <li>A closed-out operating budget</li>
-        <li>If available, form 990 from that year</li>
-      </ul></h4>
-      <h4>Once you enter your total operating expenses, a customized fee schedule will be generated that lists the minimum fees required for certification. The next step is to upload your history of fee payment in the 'Artist Payments' tab.</h4>
-    </div>
+    if (this.state.isGeneral == false) {
+      if ((this.state.isFuture || this.state.isProgress) && (this.state.isPast == null)) {
+      section = <div>
+        <h4>To process your application we will need the following documentation:</h4>
+        <ul>
+          <li>Total operating expenses for the year</li>
+          <li>A projected operating budget</li>
+        </ul>
+        <h4>Your application is effectively a commitment to paying fees according to W.A.G.E. standards. Your status will be listed as W.A.G.E. Certified Pending until the close of the fiscal period, at which time a history of fee payments is required. Once you have demonstrated your organization’s compliance you will be listed and may list yourself as W.A.G.E. Certified.
+        </h4>
+      </div>
+    } else {
+      section = <div>
+        <h4>To process your application we will need the following documentation in order to establish that fees were paid according to W.A.G.E. standards:</h4>
+        <h4>
+        <ul>
+          <li>Total operating expenses</li>
+          <li>A history of fee payment</li>
+          <li>A closed-out operating budget</li>
+          <li>If available, form 990 for {this.printYearStatus().year}</li>
+        </ul></h4>
+        <h4>Once you enter your total operating expenses, a customized fee schedule will be generated listing the minimum fees required for certification. The final step is to imput your history of fee payment under 'Artist Payments'.</h4>
+      </div>
+    }
   }
     return section
   },
