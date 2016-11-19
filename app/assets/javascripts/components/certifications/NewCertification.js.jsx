e1cf4bb37d54a286987df033c135df1f60d9f2de
Eve <eve.essex@gmail.com>
2016-11-19 15:29:13 -0500

    break year options into component

--- app/assets/javascripts/components/certifications/NewCertification.js.jsx
+++ app/assets/javascripts/components/certifications/NewCertification.js.jsx
@@ -287,46 +287,11 @@ var newCertification = React.createClass({
     } else if (this.state.contentState == 2) {
       if (this.state.editDates || (this.state.certification && !this.state.certification.fiscal_start) ) {
         if (this.state.user.fiscal_start && this.state.user.fiscal_end && !this.state.certification.fiscal_start && !this.state.certification.fiscal_end) {
-          var today = new Date
-          var years = []
-          var plusYear = ""
-          var currentYear = ""
-          function getUnique(value, index, self) {
-            return self.indexOf(value) === index;
-          }
-          // current year is valid
-          if ( (moment(this.state.user.fiscal_start) < moment(today).add(90, 'days')) && (moment(today) < moment(this.state.user.fiscal_end)) ) {
-            currentYear = moment(this.state.user.fiscal_end).format('Y')
-            years.push( parseInt(currentYear) )
-          }
-          // if upcoming year is valid
-          if (moment(this.state.user.fiscal_start).add(1, 'year') < moment(today).add(90, 'days')) {
-            if (moment(this.state.user.fiscal_start).add(2, 'years').subtract(1,'days').format('Y') != currentYear) {
-              plusYear = moment(this.state.user.fiscal_start).add(2, 'years').subtract(1,'days').format('Y')
-              years.push( parseInt(plusYear) )
-            }
-          }
-          var unique = years.filter( getUnique )
-          console.log(unique)
-          var options = unique.map( function(year) {
-           return (
-              <option key={year} value={year}>
-                {year}
-              </option>
-            )
-          })
-          var content = <div>
-            <h4>Start my certification in: </h4>
-            <div className="form certification-year">
-            <h4><select className="form-control">
-              {options}
-            </select></h4>
-            <button className="btn btn-lg" onClick={this.handleNewCertification}>Next</button>
-            </div>
-            <h4>You can apply for certification in additional years once your first application is complete.</h4>
-          </div>
+          var content = <CertificationYearOptions user={this.state.user} certification={this.state.certification} handleAddCertification={this.handleAddCertification} />
         } else {
-          var content = <FiscalDates user={this.state.user} certification={this.state.certification} editDates="true" toggleEditDates={this.toggleEditDates} formatDates={this.formatDates} handleCertificationUpdate={this.handleCertificationUpdate} handleUserUpdate={this.handleUserUpdate} handleAddCertification={this.handleAddCertification} getYearStatus={this.getYearStatus}/>
+          var content = <div>
+          <FiscalDates user={this.state.user} certification={this.state.certification} editDates="true" toggleEditDates={this.toggleEditDates} formatDates={this.formatDates} handleCertificationUpdate={this.handleCertificationUpdate} handleUserUpdate={this.handleUserUpdate} handleAddCertification={this.handleAddCertification} getYearStatus={this.getYearStatus}/>
+          </div>
         }
       } else if (this.state.certification && this.state.certification.fiscal_start) {
         var content = <div><FiscalDates user={this.state.user} certification={this.state.certification} editDates={this.state.editDates} toggleEditDates={this.toggleEditDates} formatDates={this.formatDates} handleCertificationUpdate={this.handleCertificationUpdate} handleUserUpdate={this.handleUserUpdate} handleAddCertification={this.handleAddCertification}/>
@@ -415,3 +380,95 @@ var newCertification = React.createClass({
     )
   }
 });
+
+
+var CertificationYearOptions = React.createClass({
+  handleNewCertification(e) {
+    var year = $(e.target).parent().find('select').val()
+    var newCertification = this.props.certification
+    var fiscal_start = this.props.user.fiscal_start
+    var fiscal_end = this.props.user.fiscal_end
+    if (moment(fiscal_end).format('Y') != year) {
+      if (moment(fiscal_start).format('Y') == moment(fiscal_end).format('Y')) {
+        newCertification.fiscal_start = fiscal_start.replace(moment(fiscal_start).format('Y'), e.target.value)
+      }
+    } else {
+      newCertification.fiscal_start = fiscal_start
+      newCertification.fiscal_end = fiscal_end
+    }
+    this.props.handleAddCertification(newCertification)
+  },
+  getUnique(value, index, self) {
+    return self.indexOf(value) === index;
+  },
+  setYears() {
+    var today = new Date
+    var years = []
+    var plusYear = ""
+    var currentYear = ""
+    // current year is valid
+    if ( (moment(this.props.user.fiscal_start) < moment(today).add(90, 'days')) && (moment(today) < moment(this.props.user.fiscal_end)) ) {
+      currentYear = moment(this.props.user.fiscal_end).format('Y')
+      years.push( parseInt(currentYear) )
+    }
+    // if upcoming year is valid
+    if (moment(this.props.user.fiscal_start).add(1, 'year') < moment(today).add(90, 'days')) {
+      if (moment(this.props.user.fiscal_start).add(2, 'years').subtract(1,'days').format('Y') != currentYear) {
+        plusYear = moment(this.props.user.fiscal_start).add(2, 'years').subtract(1,'days').format('Y')
+        years.push( parseInt(plusYear) )
+      }
+    }
+    var unique = years.filter( this.getUnique )
+    console.log(unique)
+    var options = unique.map( function(year) {
+     return (
+        <option key={year} value={year}>
+          {year}
+        </option>
+      )
+    })
+    return options
+  },
+  render() {
+    return (
+      <div>
+        <h4>Start my certification in: </h4>
+        <div className="form certification-year">
+        <h4><select className="form-control">
+          {this.setYears()}
+        </select></h4>
+        <button className="btn btn-lg" onClick={this.handleNewCertification}>Next</button>
+        </div>
+        <h4>You can apply for certification in additional years once your first application is complete.</h4>
+      </div>
+    )
+  }
+  // var today = new Date
+  // var years = []
+  // var plusYear = ""
+  // var currentYear = ""
+  // function getUnique(value, index, self) {
+  //   return self.indexOf(value) === index;
+  // }
+  // // current year is valid
+  // if ( (moment(this.props.user.fiscal_start) < moment(today).add(90, 'days')) && (moment(today) < moment(this.props.user.fiscal_end)) ) {
+  //   currentYear = moment(this.state.user.fiscal_end).format('Y')
+  //   years.push( parseInt(currentYear) )
+  // }
+  // // if upcoming year is valid
+  // if (moment(this.state.user.fiscal_start).add(1, 'year') < moment(today).add(90, 'days')) {
+  //   if (moment(this.state.user.fiscal_start).add(2, 'years').subtract(1,'days').format('Y') != currentYear) {
+  //     plusYear = moment(this.state.user.fiscal_start).add(2, 'years').subtract(1,'days').format('Y')
+  //     years.push( parseInt(plusYear) )
+  //   }
+  // }
+  // var unique = years.filter( getUnique )
+  // console.log(unique)
+  // var options = unique.map( function(year) {
+  //  return (
+  //     <option key={year} value={year}>
+  //       {year}
+  //     </option>
+  //   )
+  // })
+})
