02b05fd6e6844a313ac94d5322e16aaa24cfed1a
Eve <eve.essex@gmail.com>
2016-12-11 23:07:59 -0500

    precompile assets

---
+++ public/assets/components/artist_payments/table/artist_payments_table_row-d8bae7887f2ad28562788b14a9fb00a013a83c9c84777c104b56ab2d60f36557.jsx
@@ -0,0 +1,28 @@
+var ArtistPaymentsTableRow = React.createClass({
+  render() {
+    var options = this.props.fee_categories.map( function(fee_category, i) {
+      var index = i + 1
+      return (
+        <option key={index} value={index}>
+          {fee_category.name}
+        </option>
+      )
+    })
+    var cat_index = this.props.artist_payment.fee_category_id - 1
+    var fee_category_name = this.props.fee_categories[cat_index].name
+    var fee_category_floor = this.props.fee_categories[cat_index].floor_fee
+    var formatted_date = moment(this.props.artist_payment.date).format('MM/DD/Y');
+    var formatted_amount = '$' + Number(this.props.artist_payment.amount).toLocaleString();
+    var payment_row = (
+        <tr key={this.props.artist_payment.id}>
+          <td className="first">{formatted_date}</td>
+          <td>{this.props.artist_payment.artist_name}</td>
+          <td>{this.props.artist_payment.name}</td>
+          <td>{fee_category_name}</td>
+          <td>{formatted_amount} <ArtistPaymentsFeeComparison artist_payment={this.props.artist_payment} floor_fee={fee_category_floor}/></td>
+          <td>{this.props.artist_payment.check_no}</td>
+       </tr>
+     );
+    return payment_row;
+  }
+})
