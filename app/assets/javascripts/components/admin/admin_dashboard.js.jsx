0b9ad66c90cb52b2a48880b09615de4f916e5a88
eessex <eve.essex@gmail.com>
2017-01-21 23:55:41 -0500

    admin has beginnings of certifications

--- app/assets/javascripts/components/admin/admin_dashboard.js.jsx
+++ app/assets/javascripts/components/admin/admin_dashboard.js.jsx
@@ -11,11 +11,20 @@ var AdminDashboard = React.createClass({
 			<div>
         <div id="users" className="container open">
           <div className="collapse__title">
-            <h1><div className='title'><span>Certifications</span><i className='fa fa-plus'></i></div></h1>
+            <h1><div className='title'><span>Users</span><i className='fa fa-plus'></i></div></h1>
           </div>
           <div className="collapse__content">
 						<AdminUsers
 							users={this.props.users} />
+					</div>
+				</div>
+        <div id="certifications" className="container open">
+          <div className="collapse__title">
+            <h1><div className='title'><span>Certifications</span><i className='fa fa-plus'></i></div></h1>
+          </div>
+          <div className="collapse__content">
+						<AdminCertifications
+							certifications={this.props.certifications} />
 					</div>
 				</div>
 			</div>
