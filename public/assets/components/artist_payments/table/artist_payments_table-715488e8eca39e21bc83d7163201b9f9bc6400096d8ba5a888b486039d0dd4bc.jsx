15046daf7b1eaddbe038d1312820e0e8ee1e8eca
eessex <eve.essex@gmail.com>
2017-01-22 22:01:42 -0500

    precompile assets

---
+++ public/assets/components/artist_payments/table/artist_payments_table-715488e8eca39e21bc83d7163201b9f9bc6400096d8ba5a888b486039d0dd4bc.jsx
@@ -0,0 +1,162 @@
+var ArtistPaymentsTable = React.createClass({
+  getInitialState() {
+    var totals = this.totals(this.props.artist_payments)
+    return {
+      total_payments: totals.total_payments,
+      total_artists: totals.total_artists,
+      total_programs: totals.total_programs,
+      total_categories: totals.total_categories
+    }
+  },
+  sortRowsBy(event) {
+    this.props.sortRowsBy(event)
+  },
+  handleDeleteArtistPayment(artist_payment) {
+    this.props.handleDeleteArtistPayment(artist_payment);
+  },
+  handleArtistPaymentUpdate(artist_payment) {
+    var totals = this.totals(this.props.artist_payments)
+    this.setState({
+      total_payments: totals.total_payments,
+      total_artists: totals.total_artists,
+      total_programs: totals.total_programs,
+      total_categories: totals.total_categories
+    })
+    this.props.handleArtistPaymentUpdate(artist_payment)
+  },
+  paymentRow() {
+    var that = this
+    var row
+    var payment_row = this.props.artist_payments.map( function(artist_payment) {
+        if (that.props.isEdit == 'true') {
+          row = <ArtistPaymentsTableEditRow
+                  key={artist_payment.id}
+                  deleteArtistPayment={that.props.handleDeleteArtistPayment}
+                  handleArtistPaymentUpdate={that.handleArtistPaymentUpdate}
+                  artist_payment={artist_payment}
+                  fee_categories={that.props.fee_categories} />
+        } else {
+          row = <ArtistPaymentsTableRow
+                  key={artist_payment.id}
+                  deleteArtistPayment={that.props.handleDeleteArtistPayment}
+                  artist_payment={artist_payment}
+                  fee_categories={that.props.fee_categories} />
+        }
+      return row
+    })
+    return payment_row
+  },
+  totals(artist_payments) {
+    var that = this
+    artist_payments = artist_payments.map( function(artist_payment) {
+      if (artist_payment.certification_id == that.props.certification.id) {
+        return artist_payment
+      }
+    })
+    var total_payments = artist_payments.reduce(function(a, b) {
+      return a + parseInt(b.amount);
+    }, 0);
+    var all_artists = []
+    var get_artists = artist_payments.map( function(artist_payment) {
+      all_artists.push(artist_payment.artist_name)
+    })
+    var unique_artists = all_artists.filter(function(name, i) {
+      return all_artists.indexOf(name) == i;
+    });
+    var total_artists = unique_artists.length;
+
+    var all_programs = []
+    var get_programs = artist_payments.map( function(artist_payment) {
+      all_programs.push(artist_payment.name)
+    })
+    var unique_programs = all_programs.filter(function(name, i) {
+      return all_programs.indexOf(name) == i;
+    });
+    var total_programs = unique_programs.length;
+
+    var all_categories = []
+    var get_categories = artist_payments.map( function(artist_payment) {
+      all_categories.push(artist_payment.fee_category_id)
+    })
+    var unique_categories = all_categories.filter(function(id, i) {
+      return all_categories.indexOf(id) == i;
+    });
+    var total_categories = unique_categories.length;
+
+  return {
+    total_payments: total_payments,
+    total_artists: total_artists,
+    total_programs: total_programs,
+    total_categories: total_categories
+    }
+  },
+  paymentFoot() {
+    var foot
+    var format_payments
+    var format_artists
+    var format_programs
+    var format_categories
+    var has_actions
+    if (this.props.artist_payments.length > 1) {
+      format_payments = 's'
+    }
+    if (this.state.total_artists > 1) {
+      format_artists = 's'
+    }
+    if (this.state.total_programs > 1) {
+      format_programs = 's'
+    }
+    if (this.state.format_categories > 1) {
+      format_categories = 'Categories'
+    } else {
+      format_categories = 'Category'
+    }
+    if (this.props.isEdit == 'true') {
+      has_actions = <td></td>
+    }
+    if (this.props.artist_payments.length > 0) {
+      foot = <tfoot>
+          <tr>
+            <td className="sortable first" name="date">{this.props.artist_payments.length} Payment{format_payments}</td>
+            <td className="sortable" name="artist_name">{this.state.total_artists} Artist{format_artists}</td>
+            <td className="sortable" name="name">{this.state.total_programs} Program{format_programs}</td>
+            <td className="sortable" name="fee_category_id">{this.state.total_categories} {format_categories}</td>
+            <td className="total--amount" name="amount">${this.state.total_payments} Total Fees</td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tfoot>
+    }
+    return foot
+  },
+  actions() {
+    var actions
+    if (this.props.isEdit == "true") {
+      actions = <th className="actions">Actions</th>
+    }
+    return actions
+  },
+  render() {
+    var payments_table = <table id="artist_payments" className="artist-payments--table table table-responsive table-striped table-hover">
+        <thead>
+          <tr>
+            <th className="sortable first" name="date" onClick={this.sortRowsBy}>Date<i className="fa fa-sort"></i></th>
+            <th className="sortable" name="artist_name" onClick={this.sortRowsBy}>Artist Name<i className="fa fa-sort"></i></th>
+            <th className="sortable" name="name" onClick={this.sortRowsBy}>Program/Exhibition<i className="fa fa-sort"></i></th>
+            <th className="sortable" name="fee_category_id" onClick={this.sortRowsBy}>Fee Category<i className="fa fa-sort"></i></th>
+            <th className="sortable" name="amount" onClick={this.sortRowsBy}>Amount<i className="fa fa-sort"></i></th>
+            <th className="sortable" name="check_no" onClick={this.sortRowsBy}>Check #<i className="fa fa-sort"></i></th>
+            <th name="notes">Notes</th>
+            {this.actions()}
+          </tr>
+        </thead>
+        {this.paymentFoot()}
+        <tbody>
+          {this.paymentRow()}
+        </tbody>
+      </table>
+    return (
+      payments_table
+    );
+  }
+});
---
+++ public/assets/components/fee_tracker/fee_tracker-04fd124c3f26f6c36c9f76d2f1f7dbb372246fd0b266fb848ec1d6ad8a6dd0f3.jsx
@@ -0,0 +1,116 @@
+ var FeeTracker = React.createClass({
+  getInitialState() {
+    return {
+      showForm: true,
+      errors: {}
+    }
+  },
+  toggleQbPl() {
+    this.setState({ showForm: false })
+  },
+  toggleTracker() {
+    this.setState({ showForm: true })
+  },
+  showForms() {
+    var disabled
+    var subtitle
+    if (this.props.yearStatus == 'future' || this.props.new_user || !this.props.certification.operating_expenses) {
+      disabled = ' disabled'
+      if (!this.props.certification.operating_expenses) {
+        subtitle = <span className='subtitle'>* complete your <a onClick={this.props.goFiscalDetails}>Fiscal Details</a> to begin using the Fee Tracker.</span>
+      }
+    }
+    if (this.state.showForm) {
+      if (this.props.yearStatus == 'current' && this.props.new_user) {
+        subtitle = <span className='subtitle'>* submit your application to begin using the Fee Tracker.</span>
+      }
+      if (this.props.yearStatus == 'future') {
+        subtitle = <span className='subtitle'>* when your fiscal year has started</span>
+      }
+      return (
+        <div className={'fee-tracker__payments new-form ' + disabled}>
+          <div className='title'>
+            <h3>Create A New Payment{subtitle}</h3>
+            <h6 onClick={this.toggleQbPl}>Upload Quickbooks File</h6>
+          </div>
+          <FeeTrackerNew
+            new_user={this.props.new_user}
+            yearStatus={this.props.yearStatus}
+            certification={this.props.certification}
+            fee_categories={this.props.fee_categories}
+            handleAddArtistPayment={this.props.handleAddArtistPayment} />
+        </div>
+      )
+    } else {
+      if (this.props.yearStatus == 'current') {
+        subtitle = <span className='subtitle'>* at the close of your fiscal year</span>
+      }
+      return (
+        <div className={'fee-tracker__payments ql-pl' + disabled}>
+        <div className='title'>
+          <h3>Upload Quickbooks File{subtitle}</h3>
+          <h6 onClick={this.toggleTracker}>Create A New Payment</h6>
+        </div>
+         {this.formQbPl()}
+        </div>
+      )
+    }
+  },
+  formQbPl() {
+    var disabled
+    if (this.props.yearStatus == 'future' || this.props.yearStatus == 'current') {
+      disabled = true
+    } else {
+      disabled = false
+    }
+    var qb_pl = <UploadFile
+        model={this.props.certification}
+        required='true'
+        type='qb_pl'
+        disabled={disabled}
+        handleFileUpdate={this.props.handleCertificationUpdate}
+        accept='application/pdf' />
+    return qb_pl
+  },
+  paymentsTable() {
+    var payments
+    if (this.state.showForm) {
+      if (this.props.artist_payments && this.props.artist_payments.length > 0) {
+        payments = <div className="section artist-payments clearfix">
+          <h3 className="section artist-payments__title">Artist Payments</h3>
+          <ArtistPaymentsTable
+            artist_payments={this.props.artist_payments}
+            sortRowsBy={this.props.sortRowsBy}
+            paymentsSorted={this.props.paymentsSorted}
+            isEdit="true"
+            certification={this.props.certification}
+            handleArtistPaymentUpdate={this.props.handleArtistPaymentUpdate}
+            handleDeleteArtistPayment={this.props.handleDeleteArtistPayment}
+            fee_categories={this.props.fee_categories} /></div>
+      }
+    }
+    return payments
+  },
+  showIntro() {
+    if (this.props.showIntro) {
+      return (
+        <FeeTrackerText
+          user={this.props.user}
+          new_user={this.props.new_user}
+          yearStatus={this.props.yearStatus}
+          certification={this.props.certification}
+          goFeeSchedule={this.props.goFeeSchedule}
+          toggleTracker={this.toggleTracker} />
+      )
+    }
+  },
+  render() {
+    return (
+      <div className="fee-tracker fee-tracker--new">
+        {this.showIntro()}
+        {this.showForms()}
+        {this.paymentsTable()}
+      </div>
+      );
+    }
+  });
